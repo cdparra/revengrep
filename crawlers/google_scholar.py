048da90f8b7a7df29c56929590861792f63690b3
yaretsd@gmail.com <yaretsd@gmail.com@d0308b73-babe-94d5-f1e8-81278de00bd6>
2011-03-24 04:39:36 +0000

    h-index comparing

--- crawlers/google_scholar.py
+++ crawlers/google_scholar.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3.1
-import sys
 
-# This is the first attempt to create a some kind of spider/crawler 
+import sys
+import google.scholar
+import re
+
+# This is the first attempt to create a same kind of spider/crawler 
 # for fetching useful information about specified scientist from
 # Google Scholar system
 
@@ -16,19 +19,85 @@ def matchName(rname, qname):
 def parseName(name):
 # transmit the input author name into the usable version
   # TODO: add here some logic
-  return name  
+  return name.replace("_", " ") 
 
+
+def hIndex(cit):
+# h-index
+# A scientist has index h if h of [his/her] Np papers have at least h citations
+# each, and the other (Np − h) papers have at most h citations each.
+  for i in range(0, len(cit)):
+    if i+1 > cit[i]:
+      return i
+  return len(cit)   
+    
+def gIndex(cit):
+# g-index
+# Given a set of articles ranked in decreasing order of the number of citations
+# that they received, the g-index is the (unique) largest number such that
+# the top g articles received (together) at least g^2 citations.     
+  s = 0
+  for i in range(0, len(cit)):
+    s += cit[i]
+    if s < i*i:
+      return i
+  return len(cit)  
+    
+          
+
+def getCitationList(name, perPage, numPages):
+  engine = google.scholar.Engine(name, perPage, numPages)
+  
+  print(engine)
+  
+  cit = []  
+  citre = re.compile(r"Cited by (?P<cit>[\d]+)");
+    
+  for page in engine:
+    ma = map(lambda m: int(m.group("cit")), citre.finditer(page))
+    cit.extend(list(ma))
+
+  cit.sort()
+  cit.reverse()
+  
+  print("num_cit = {0}".format(len(cit)))
+#  print("cit = {0}".format(cit))
+  
+  return cit   
+    
 
 
 def main():
-  rname = parseName(sys.argv[1])
+  fin = sys.argv[1]
+  fout = sys.argv[2]
+  perPage = int(sys.argv[3]) if len(sys.argv) >= 4 else 10
+  numPages = int(sys.argv[4]) if len(sys.argv) >= 5 else 10
   
-  
+  res = []
+  with open(fin, "r") as f:
+    for line in f:
+      srh, name = line.strip().split(" ")
+      rh = int(srh)
+      print("{0} is processed...".format(name.replace("_", " ")))
+      cit = getCitationList(parseName(name), perPage, numPages)
+      h = hIndex(cit)
+      dif = abs(rh - h)
+      print("processing is ready, rh = {0:3d} h = {1:3d} dif = {2}"
+      .format(rh, h, dif))
+      res.append((rh, h, name, dif))
+      
+  sDif = 0    
+  with open(fout, "w") as f:
+    for rh, h, name, dif in res:
+      sDif += dif
+      f.write("rh = {0:3d} h = {1:3d} dif = {2:2d}  {3}\n".format(rh, h, dif, name))
+    f.write("average dif = {0:.5f}\n".format(sDif / len(res)))  
+ 
   
 
 def badArgs():
 # not enough arguments are present
-  print("Usage: {0} <author_name> [<params>]".format(__file__))
+  print("Usage: {0} <input> <output> [<per_page>] [<num_pages>] [<params>]".format(__file__))
   sys.exit(1)
 
 if __name__ == "__main__":
---
+++ crawlers/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "calculating h-index..."
+#time ./google_scholar.py authors.txt results.txt 100 3
+time ./google_scholar.py short.txt results.txt 100 2
+
