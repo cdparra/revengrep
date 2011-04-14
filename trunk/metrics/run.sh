753d925c279e9e55ab7f9d2137ea6a01d92a4bd6
yaretsd@gmail.com <yaretsd@gmail.com@d0308b73-babe-94d5-f1e8-81278de00bd6>
2011-04-14 07:47:23 +0000

    random scientist's metrics generator

--- trunk/metrics/run.sh
+++ trunk/metrics/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #./correlation.py short.txt correlation.txt
-./correlation.py rankings.txt correlation.txt
+#./correlation.py rankings.txt correlation.txt
+./correlation.py randrankings.txt randcorrelation.txt
