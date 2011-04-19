828cdb5c25bdd13de2fd486fa5fcc347eab95302
yaretsd@gmail.com <yaretsd@gmail.com@d0308b73-babe-94d5-f1e8-81278de00bd6>
2011-04-19 12:39:35 +0000

    kendall correlation results

--- trunk/metrics/calculator.py
+++ trunk/metrics/calculator.py
@@ -16,6 +16,7 @@ Python3.1
 import sys
 import math
 from functools import reduce
+import statistics
 
 metrics = ("gs_pub", "gs_citedpub", "gs_citations", "gs_hindex", "gs_gindex", "gs_aindex", "gs_h2index", "gs_eindex")
 
@@ -50,19 +51,8 @@ def extractMetrics(people, mask):
   ms = filter(lambda i: (mask&(1<<i)), range(0, len(metrics)))    
   return list(map(lambda i: extractMetric(people, metrics[i]), ms))    
   
-def mean(x):
-  """Mean value."""
-  y = list(x)
-  return 1.0*sum(y)/len(y)  
   
-def correlation(x, y):
-  """Calc the correlation between x and y."""
-  mx, my = mean(x), mean(y)
-  mx2, my2 = mean(map(lambda a, b: a*b, x, x)), mean(map(lambda a, b: a*b, y, y))
-  mxy = mean(map(lambda a, b: a*b, x, y))
-  return (mxy-mx*my)/(math.sqrt(mx2 - mx**2)*math.sqrt(my2-my**2))    
-  
-def allSubsets(people):
+def allSubsets(people, calcCorrelation):
   """Try all subsets and find the correlation."""
   ranks = extractMetric(people, "rank")
   result = []
@@ -72,32 +62,43 @@ def allSubsets(people):
     # find a mean vector
     sm = reduce(lambda x, y: [x[i]+y[i] for i in range(0, len(x))], metrcs, empty)
     snm = [x/len(metrcs) for x in sm]
-    cor = correlation(ranks, snm)
+    cor = calcCorrelation(ranks, snm)
     print("{0} = {1:5f}".format(mask, cor))
     result.append((cor, mask))
   result.sort(key=lambda x: abs(x[0]));
   result.reverse()  
   return result    
-      
-def main():              
-  inFile = sys.argv[1]
-  outFile = sys.argv[2]
   
-  people = inputData(inFile)
-  result = allSubsets(people)
-
-  with open(outFile, "w") as fout:
+def printResult(result, fileName):
+  with open(fileName, "w") as fout:
     for x, mask in result:
       ms = filter(lambda i: (mask&(1<<i)), range(0, len(metrics)))
       sts = map(lambda i: metrics[i], ms)
-      fout.write("{0:5f} ==> {1}\n".format(x, list(sts)))  
+      fout.write("{0:5f} ==> {1}\n".format(x, list(sts)))   
+  
+def calcPearson(people):
+  print("Pearson's correlation:")
+  result = allSubsets(people, statistics.correlationPearson)
+  printResult(result, "pearson_corr.txt")
+  
+def calcKendall(people):
+  print("Kendall's correlation:")
+  result = allSubsets(people, statistics.correlationKendallTau)
+  printResult(result, "kendall_corr.txt")    
+      
+def main():              
+  inFile = sys.argv[1]    
+  people = inputData(inFile)  
+  
+  calcPearson(people)
+  calcKendall(people)
 
 def badArgs():
 # not enough arguments are present
-  print("Usage: {0} <input> <output>".format(__file__))
+  print("Usage: {0} <input>".format(__file__))
   sys.exit(1)
 
 if __name__ == "__main__":
-  if len(sys.argv) < 3:
+  if len(sys.argv) < 2:
     badArgs()
   main()
