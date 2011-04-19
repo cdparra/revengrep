dcece6d026a8d55a2f64d940f4ca35589a2255ee
yaretsd@gmail.com <yaretsd@gmail.com@d0308b73-babe-94d5-f1e8-81278de00bd6>
2011-04-19 12:39:35 +0000

    kendall correlation results

--- metrics/run.sh
+++ metrics/run.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-#./correlation.py short.txt correlation.txt
-#./correlation.py rankings.txt correlation.txt
-./correlation.py randrankings.txt randcorrelation.txt
+./calculator.py rankings.txt
